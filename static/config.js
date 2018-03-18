4629edc6f9c7e6f16c6cf34d18a3777287a325bd
Drethin <37494146+Drethin@users.noreply.github.com>
2018-03-18 00:58:40 -0400

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,18 +2,18 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1rQ7oRvCVX7mmW3qWUqNP99Zoy2gS9pbs0a-3DR-Zsi8";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "4356-3933-1757";
+var inGameName = "Drethin";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/drethin";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
